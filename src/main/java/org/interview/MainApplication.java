4f46a0cf281412715417847cb6c45b44a83d30fc
zhanghao <anlu.zhangcs@gmail.com>
2023-06-28 10:44:41 +0800

    init project

---
+++ src/main/java/org/interview/MainApplication.java
@@ -0,0 +1,18 @@
+package org.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Manny
+ * @create 2023-06-28-10:23
+ */
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
