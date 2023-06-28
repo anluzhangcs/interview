706d8d3732b005781f82e865a5916c7c4a799014
manny <manny@email>
2023-06-28 15:41:21 +0800

    config mapper path

--- src/main/java/org/interview/MainApplication.java
+++ src/main/java/org/interview/MainApplication.java
@@ -1,5 +1,6 @@
 package org.interview;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("org.interview.mapper")
 public class MainApplication {
 
     public static void main(String[] args) {
